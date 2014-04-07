0791330fa74eeb404da063d6fb1bc0a7cf596461
Daniel Bulla <daniel.bulla@alumni.fh-aachen.de>
2014-04-07 16:43:37 +0200

    cleanup unused old directories.

--- advanced_example/obj-0.1/obj.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef OBJ_HPP_INCLUDED
-#define OBJ_HPP_INCLUDED
-
-#include <obj-0.1/obj/config.hpp>
-#include <obj-0.1/obj/obj.hpp>
-#include <obj-0.1/obj/obj_parser.hpp>
-
-#endif // OBJ_HPP_INCLUDED
